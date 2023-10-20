138c67227a25939ffb3fdae38ff35a2ff3857afa
carolinamoralese <caromorales1396@gmail.com>
2023-10-20 15:31:13 -0500

    se crea estado para mostrar varias imagenes

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
+'use client';
+import { useState } from "react";
 import { RandomFox } from "../components/RandomFox";
 
 const random = () => Math.floor(Math.random() * 123) + 1;
 
-export default function Home() {
+ function Home() {
+  const [images, setImagenes] = useState([
+    `https://randomfox.ca//images//${random()}.jpg`,
+    `https://randomfox.ca//images//${random()}.jpg`,
+    `https://randomfox.ca//images//${random()}.jpg`,
+    `https://randomfox.ca//images//${random()}.jpg`,
+  ]);
   return (
     <div>
       <head>
@@ -10,8 +18,14 @@ export default function Home() {
       </head>
       <main>
         <h1 className="text-3xl font-bold underline">HOLA</h1>
-        <RandomFox image={`https://randomfox.ca//images//${random()}.jpg`}/>
+        {images.map((image, index) => (
+          <div key={index}className="p4">
+          <RandomFox image={image} />
+          </div>
+        ))}
       </main>
     </div>
   );
 }
+
+export default (Home); // Marcar el componente como cliente
