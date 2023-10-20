caebd719b4162552026fe39ccceec9c007670552
carolinamoralese <caromorales1396@gmail.com>
2023-10-20 14:39:02 -0500

    se crea componente de imagenes aleatorias

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import { RandomFox } from "../components/RandomFox";
+
+
+export default function Home() {
+  return (
+    <div>
+      <head>
+        <title>Proyecto</title>
+      </head>
+      <main>
+        <h1 className="text-3xl font-bold underline">HOLA</h1>
+        <RandomFox/>
+      </main>
+    </div>
+  );
+}
---
+++ components/RandomFox.tsx
@@ -0,0 +1,7 @@
+// genere un numero random
+const random = () => Math.floor(Math.random() * 123) +1
+
+export const RandomFox = (): JSX.Element =>{
+    const image:string = `https://randomfox.ca//images//${random()}.jpg`
+    return <img width={320} height="auto" src={image} className="rounded"/>
+}
