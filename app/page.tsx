1374139f0fef267650c2b61d66f7984d9a63a280
carolinamoralese <caromorales1396@gmail.com>
2023-10-20 16:10:18 -0500

    se crea boton para agregar imagenes para que el usuario pueda interactuar con la pagina

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,31 @@
 'use client';
 import { useState } from "react";
+import type { MouseEventHandler, } from "react";
 import { RandomFox } from "../components/RandomFox";
 
 const random = () => Math.floor(Math.random() * 123) + 1;
 
+//generar ID unicos
+const genererId = () => Math.random().toString(36).substring(2,9)
+
+type ImageItems = {id:string; url:string}
+
  function Home() {
-  const [images, setImagenes] = useState([
-    `https://randomfox.ca//images//${random()}.jpg`,
-    `https://randomfox.ca//images//${random()}.jpg`,
-    `https://randomfox.ca//images//${random()}.jpg`,
-    `https://randomfox.ca//images//${random()}.jpg`,
-  ]);
+  const [images, setImagenes] = useState<Array<ImageItems>>([]);
+
+const addNewImage: MouseEventHandler<HTMLButtonElement> = (event) => {
+
+  const newImageItem: ImageItems = {
+    id:genererId(),
+    url:`https://randomfox.ca//images//${random()}.jpg`
+  }
+
+  setImagenes([
+    ...images,
+    newImageItem
+  ])
+}
+
   return (
     <div>
       <head>
@@ -18,9 +33,10 @@ const random = () => Math.floor(Math.random() * 123) + 1;
       </head>
       <main>
         <h1 className="text-3xl font-bold underline">HOLA</h1>
-        {images.map((image, index) => (
-          <div key={index}className="p4">
-          <RandomFox image={image} />
+        <button onClick={addNewImage}>Agregar nueva imagen</button>
+        {images.map(({id, url}) => (
+          <div key={id}className="p-4">
+          <RandomFox image={url} />
           </div>
         ))}
       </main>
