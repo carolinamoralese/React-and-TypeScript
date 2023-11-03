da15fa96b92d3dce2b1c1dc21c4e37156d37172f
carolinamoralese <caromorales1396@gmail.com>
2023-11-03 16:46:40 -0500

    se agregan estilos a las imagenes

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,26 @@
-'use client';
+"use client";
 import { useState } from "react";
-import type { MouseEventHandler, } from "react";
+import type { MouseEventHandler } from "react";
 import { RandomFox } from "../components/RandomFox";
 
 const random = () => Math.floor(Math.random() * 123) + 1;
 
 //generar ID unicos
-const genererId = () => Math.random().toString(36).substring(2,9)
+const genererId = () => Math.random().toString(36).substring(2, 9);
 
-type ImageItems = {id:string; url:string}
+type ImageItems = { id: string; url: string };
 
- function Home() {
+function Home() {
   const [images, setImagenes] = useState<Array<ImageItems>>([]);
 
-const addNewImage: MouseEventHandler<HTMLButtonElement> = (event) => {
+  const addNewImage: MouseEventHandler<HTMLButtonElement> = (event) => {
+    const newImageItem: ImageItems = {
+      id: genererId(),
+      url: `https://randomfox.ca//images//${random()}.jpg`,
+    };
 
-  const newImageItem: ImageItems = {
-    id:genererId(),
-    url:`https://randomfox.ca//images//${random()}.jpg`
-  }
-
-  setImagenes([
-    ...images,
-    newImageItem
-  ])
-}
+    setImagenes([...images, newImageItem]);
+  };
 
   return (
     <div>
@@ -32,16 +28,26 @@ const addNewImage: MouseEventHandler<HTMLButtonElement> = (event) => {
         <title>Proyecto</title>
       </head>
       <main>
-        <h1 className="text-3xl font-bold underline">HOLA</h1>
-        <button onClick={addNewImage}>Agregar nueva imagen</button>
-        {images.map(({id, url}) => (
-          <div key={id}className="p-4">
-          <RandomFox image={url} />
-          </div>
-        ))}
+        <h1 className="text-3xl font-bold text-center p-4">
+          Imagenes random de zorros
+        </h1>
+        <div className="text-center">
+        <button
+        onClick={addNewImage}
+        className="bg-gray-500 text-white px-4 py-2 rounded-md"
+        >Agregar nueva imagen</button>
+        </div>
+       
+        <div className="flex flex-wrap justify-center">
+          {images.map(({ id, url }) => (
+            <div key={id} className="p-4">
+              <RandomFox image={url} />
+            </div>
+          ))}
+        </div>
       </main>
     </div>
   );
 }
 
-export default (Home); // Marcar el componente como cliente
+export default Home; // Marcar el componente como cliente
--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,8 +1,17 @@
-// genere un numero random
+import { useRef } from "react";
 
 type Props = {image:string}
 
 export const RandomFox = ({image}:Props): JSX.Element => {
+    const node = useRef<HTMLImageElement>(null)
  
-  return <img width={320} height="auto" src={image} className="rounded" />;
+  return (
+    <div className="flex items-center justify-center">
+      <div className="w-48 h-48 p-4 rounded-md shadow-md">
+      <img ref={node} src={image} className="w-full h-full rounded" 
+      />
+      </div>
+    </div>
+ 
+  )
 };
